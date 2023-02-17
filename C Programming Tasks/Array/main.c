f5f024a60e8a3cd6499636072f1ec3384e70ae28
Menna Eid <90302774+2mennaeid@users.noreply.github.com>
2023-02-17 13:50:08 +0200

    Add files via upload

---
+++ C Programming Tasks/Array/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*void mean(int arr[])
+{
+    int sum=0;
+    int average;
+    int _size= sizeof(arr) / sizeof(arr[0]);
+    for(int i=0;i<_size;i++)
+    {
+        sum +=arr[i];
+    }
+    average = (sum)/(_size);
+    for(int i=0;i<_size;i++)
+    {
+        printf("%d\n",arr[i]);
+    }
+    printf("\n%d\n",sum);
+    printf("%d",average);
+}*/
+int main()
+{
+    int arr[]={1,2,3,4,5};
+    int sum=0;
+    int average;
+    int _size= sizeof(arr) / sizeof(arr[0]);
+    for(int i=0;i<_size;i++)
+    {
+        sum +=arr[i];
+    }
+    average = (sum)/(_size);
+    for(int i=0;i<_size;i++)
+    {
+        printf("%d\n",arr[i]);
+    }
+    printf("\n%d\n",sum);
+    printf("%d",average);
+    return 0;
+}
+
+
